3cb1885aab46ffc7fdc385338bf651b7f4e699a8
SaiNageswarS <saisat@microsoft.com>
2023-08-20 17:32:36 +0530

    Adding embedding and embedding upload

--- ChunkPdf.py
+++ ChunkPdf.py
@@ -1,3 +1,4 @@
+import os
 import tika
 tika.initVM()
 from tika import parser
@@ -62,5 +63,22 @@ def chunk_pdf(pdf_path="MedicalHistory/Camphor Poisoning.pdf"):
     parsed_pdf = parser.from_file(pdf_path)
     content = parsed_pdf["content"]
     paragraphs = __get_clean_paragraphs__(content)
+
     return paragraphs
 
+
+if __name__ == '__main__':
+    base_path = "MedicalHistory/"
+    file_list = os.listdir(base_path)
+    pdf_file_list = [x for x in file_list if x.endswith(".pdf")]
+    chunked_files = [chunk_pdf(base_path + x) for x in pdf_file_list]
+
+    if not os.path.exists("MedicalHistoryPreProcessed"):
+        os.makedirs("MedicalHistoryPreProcessed")
+
+    for filename, chunked_file in zip(pdf_file_list, chunked_files):
+        with open("MedicalHistoryPreProcessed/" + filename.replace(".pdf", ".txt"), "w",
+                  encoding="utf-8") as output_file:
+            for para in chunked_file:
+                output_file.write(para)
+                output_file.write("\n\n")
--- EmbedPdf.py
+++ EmbedPdf.py
@@ -1,27 +1,81 @@
 import os
-from ChunkPdf import chunk_pdf
+
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer('all-mpnet-base-v2')
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import pinecone
+pinecone.init(
+    api_key=os.environ.get("PINECONE_API_KEY"),
+    environment=os.environ.get("PINECONE_ENVIRONMENT"))
+
+PINECONE_INDEX = "kotlang-vectordb"
+PINECONE_COLLECTION = "medical-ert-guideline"
+# pinecone.create_index(name=PINECONE_INDEX, dimension=768, metric="cosine")
+index = pinecone.Index(PINECONE_INDEX)
 
 
-def __process_pdfs_to_txt__(base_path="MedicalHistory/"):
-    file_list = os.listdir(base_path)
-    pdf_file_list = [x for x in file_list if x.endswith(".pdf")]
-    chunked_files = [chunk_pdf(base_path + x) for x in pdf_file_list]
+def __get_keyword__(filename):
+    keyword = filename.split(".")[0]
+    keyword = keyword.replace("_", " ").replace("-", " ")
+    keyword = keyword.\
+        replace("guidelines", "").\
+        replace("guideline", "").\
+        replace("chart", "").\
+        replace("factsheet", "").\
+        replace("Guideline", "").\
+        replace("Fact sheet", "")
 
-    if not os.path.exists("MedicalHistoryPreProcessed"):
-        os.makedirs("MedicalHistoryPreProcessed")
+    keyword = keyword.lower()
+    if "poisoning" not in keyword:
+        keyword = keyword.strip() + " poisoning"
 
-    for filename, chunked_file in zip(pdf_file_list, chunked_files):
-        with open("MedicalHistoryPreProcessed/" + filename.replace(".pdf", ".txt"), "w", encoding="utf-8") as output_file:
-            for para in chunked_file:
-                output_file.write(para)
-                output_file.write("\n\n")
-
-    return chunked_files
+    return keyword.strip()
 
 
-def embed_pdfs():
-    __process_pdfs_to_txt__()
+def embed_file(file_path="MedicalHistoryPreProcessed/Camphor Poisoning.txt"):
+    with open(file_path, "r", encoding="utf-8") as file:
+        file_content = file.read()
+        paras_original = file_content.split("\n\n")
+
+        filename = os.path.basename(file_path)
+        keyword = __get_keyword__(filename)
+        paras_for_embedding = [keyword + " " + x for x in paras_original]
+
+        if len(paras_for_embedding) == 0:
+            print("No paras in " + file_path)
+            return []
+
+        print("Embedding " + filename)
+        para_embeddings = model.encode(paras_for_embedding)
+        print("Embedding Finished. Vector Size : " + str(len(para_embeddings[0])))
+
+        pinecone_requests = []
+        idx = 0
+
+        for vec, text in zip(para_embeddings, paras_original):
+            pinecone_request = {
+                'id': keyword + "-" + str(idx),
+                'values': vec.tolist(),
+                'metadata': {'text': text, 'reference': filename.replace(".txt", ".pdf")}
+            }
+
+            pinecone_requests.append(pinecone_request)
+            idx += 1
+
+        upsert_response = index.upsert(
+            vectors=pinecone_requests,
+            namespace=PINECONE_COLLECTION)
+
+        print(upsert_response)
+        return para_embeddings
 
 
 if __name__ == '__main__':
-    embed_pdfs()
+    base_path = "MedicalHistoryPreProcessed/"
+    file_list = os.listdir(base_path)
+
+    for file in file_list:
+        embed_file(base_path + file)
